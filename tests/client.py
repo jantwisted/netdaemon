af04ea8fbb61075c429d73bd90189d9a234a5e46
Janith <liyanage.janith.perera@sap.com>
2018-11-19 18:22:31 +0100

    test scripts added

---
+++ tests/client.py
@@ -0,0 +1,20 @@
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = socket.gethostname()                           
+
+port = 9999
+
+msg = 'Hello World'
+
+# connection to hostname on the port.
+s.connect((host, port))                               
+s.send(msg.encode('ascii'))
+# Receive no more than 1024 bytes                                    
+
+s.close()
+
+
